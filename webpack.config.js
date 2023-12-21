7c2610700e568b7213f8d8471232a364401397bc
Ross Terker <ross.terker@gmail.com>
2023-12-20 22:55:32 -0500

    config: initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    clean: true,
+    publicPath: '/build/'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env'], 
+              ['@babel/preset-react']
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './'),
+      publicPath: '/'
+    },
+    proxy: {
+      '/user': 'http://localhost:3000',
+      '/appointment': 'http://localhost:3000'
+    }
+  }
+};
