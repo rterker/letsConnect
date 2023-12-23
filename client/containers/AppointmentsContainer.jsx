1e1bc4031fb8c2eae087630dc38bfba265d72cea
Ross Terker <ross.terker@gmail.com>
2023-12-23 18:16:03 -0500

    feat: got fetches in Main and Appointments containers to work. needed to add loading state for conditional rendering

--- client/containers/AppointmentsContainer.jsx
+++ client/containers/AppointmentsContainer.jsx
@@ -1,16 +1,26 @@
 import React, { useEffect, useState } from "react";
 
-const AppointmentsContainer = ({ user, origin }) => {
-  //fetch appointments and render them
+const AppointmentsContainer = ({ user, BASE_URL }) => {
   const [appointments, setAppointments] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch(`${origin}/appointment/user`);
+    fetch(`${BASE_URL}/appointments/${user._id}`)
+      .then(appointments => appointments.json())
+      .then(appointments => {
+        setAppointments(appointments);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(`The following error occured in AppointmentsContainer fetch: ${err}`));
   //may need to add appointments to dependencies if they don't update correctly
   }, []);
 
+  if (isLoading) return <div>'...isLoading'</div>
+
   return (
-    <div className="bg-gray-300 w-72"></div>
+    <div className="bg-gray-300 w-72">
+      {appointments.map(appointment => <div>{appointment.subject}</div>)}
+    </div>
   );  
 }
 
--- client/containers/MainContainer.jsx
+++ client/containers/MainContainer.jsx
@@ -2,27 +2,34 @@ import React, { useEffect, useState } from "react";
 import AppointmentsContainer from "./AppointmentsContainer.jsx"
 import BodyContainer from "./BodyContainer.jsx"
 
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = 'http://localhost:8080';
+const userId = '657f205517722b44fe9fda33';
 
 const MainContainer = () => {
   const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    function getUser(id) {
-      fetch(`${BASE_URL}/user/${id}`)
-        .then(user => user.json())
-        .then(user => setUser(user))
-        .catch(err => console.log(`The following error occured in getUser: ${err}`));
-    } 
-    getUser('657f205517722b44fe9fda33');
+    fetch(`${BASE_URL}/user/${userId}`)
+      .then(user => user.json())
+      .then(user => {
+        setUser(user);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(`The following error occured in MainContainer getUser fetch: ${err}`));
   }, []);
+
+
+  if (isLoading) return <div>'loading....'</div>;
 
   return (
     <>
-      <AppointmentsContainer user={user} origin={BASE_URL}/>
-      <BodyContainer user={user} origin={BASE_URL}/>
+      <AppointmentsContainer user={user} BASE_URL={BASE_URL}/>
+      <BodyContainer user={user} BASE_URL={BASE_URL}/>
     </>
   );
+
 }
 
 export default MainContainer;
+
