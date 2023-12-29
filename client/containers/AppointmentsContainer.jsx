b4e736a221b637d80bc4081b990f8a2bf17ed88f
Ross Terker <ross.terker@gmail.com>
2023-12-29 12:36:46 -0500

    refactor: changed appointments sorting to ascending in AppointmentsContainer

--- client/containers/AppointmentsContainer.jsx
+++ client/containers/AppointmentsContainer.jsx
@@ -4,11 +4,11 @@ import Appointment from '../components/Appointment.jsx';
 //called by: MainContainer
 const AppointmentsContainer = ({ user, BASE_URL, appointments, setActiveAppointmentId, activeAppointmentId, setPreviousActiveAppointmentId }) => {
   const appointmentTimestamps = appointments.map(el => ({ ...el, date: Date.parse(el.date)}));
-  const sortedAppointmentsDesc = appointmentTimestamps.toSorted((first, second) => second.date - first.date);
+  const sortedAppointmentsAsc = appointmentTimestamps.toSorted((first, second) => first.date - second.date);
 
   return (
     <ol className="flex flex-col bg-[#f7f6e7] w-72 gap-2 overflow-auto text-center pt-3">
-      {sortedAppointmentsDesc.map(appointment => <Appointment appointment={appointment} setActiveAppointmentId={setActiveAppointmentId} activeAppointmentId={activeAppointmentId} setPreviousActiveAppointmentId={setPreviousActiveAppointmentId} key={appointment._id} />)}
+      {sortedAppointmentsAsc.map(appointment => <Appointment appointment={appointment} setActiveAppointmentId={setActiveAppointmentId} activeAppointmentId={activeAppointmentId} setPreviousActiveAppointmentId={setPreviousActiveAppointmentId} key={appointment._id} />)}
     </ol>
   );  
 }
