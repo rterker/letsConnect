4e313951edcaea6fe92ac5bfd0291cc66e3cccd7
Ross Terker <ross.terker@gmail.com>
2023-12-30 15:10:00 -0500

    refactor: add 1 to month displayed in calendar header

--- client/components/CalendarStartP.jsx
+++ client/components/CalendarStartP.jsx
@@ -37,7 +37,7 @@ const CalendarStart = ({ currentDate, appointments, setActiveDay }) => {
     <div className="flex flex-col mx-auto my-auto w-160 h-160 bg-white animate-slideIn">
       <div className="flex justify-between pt-3 pb-3 border-t-2 border-l-2 border-r-2 border-black bg-[#fafafa]">
         <button className="w-10 bg-[#c1c0b9] text-xl rounded-lg shadow-lg hover:shadow-xl active:shadow-inner focus:outline-none ml-2 cursor-pointer">{'<'}</button>
-        <b className="text-xl">{currentDateObj.monthOfAppointment} / {currentDateObj.yearOfAppointment}</b>
+        <b className="text-xl">{currentDateObj.monthOfAppointment + 1} / {currentDateObj.yearOfAppointment}</b>
         <button className="w-10 bg-[#c1c0b9] text-xl rounded-lg shadow-lg hover:shadow-xl active:shadow-inner focus:outline-none mr-2 cursor-pointer">{'>'}</button>
       </div>
       <div className={`grid grid-rows-${gridRows} grid-cols-7 w-full h-full border border-black`}>
