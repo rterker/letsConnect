d028d63db783ac38639075d54db6d382304a33e9
Ross Terker <ross.terker@gmail.com>
2023-12-30 14:13:42 -0500

    ui refactor: removed flex-wrap to prevent div from growing when there are too many dots

--- client/components/CalendarDayP.jsx
+++ client/components/CalendarDayP.jsx
@@ -17,12 +17,13 @@ const CalendarDay = ({ dateOfMonth, appointmentsForDay, setActiveDay }) => {
     );
   }
 
+  //TO DO: handle overflowing dots; used to have flex-wrap, but the div grew out of proportion
   return (
     <div className="flex flex-col bg-[#fafafa] border border-black cursor-pointer hover:bg-[#c1c0b9] active:bg-[#537791] active:text-[#f7f6e7]" onClick={(e) => handleClick(e)}>
       <div>
       {dateOfMonth}
       </div>
-      <div className="flex grow justify-center items-center gap-1 flex-wrap">
+      <div className="flex grow justify-center items-center gap-1">
         {appointmentsForDay.map(appt => {
           if (appt) return <Dot/>;
           return;
