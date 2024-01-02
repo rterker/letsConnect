670774833115f583e14cc6458975869763cbeb7b
Ross Terker <ross.terker@gmail.com>
2024-01-01 22:00:59 -0500

    feat: handleChange in edit mode

--- client/components/ViewAndEditAppointment.jsx
+++ client/components/ViewAndEditAppointment.jsx
@@ -2,26 +2,27 @@ import React, { useState } from "react";
 
 //called by: ActiveDay
 const ViewAndEditAppointment = ({ appointment, inEditMode }) => {
-  const { date, subject, participants, status, creator, createdAt} = appointment;
-  //TO DO: use state to load inital value in input fields and then to handle changes
-  const [formData, setFormDate] = useState();
-  let formattedDate = new Date(date).toLocaleString();
+  // const { date, subject, participants, status, creator, createdAt} = appointment;
+  // let formattedDate = new Date(date).toLocaleString();
+  let formattedDate = new Date(appointment.date).toLocaleString();
+  const appointmentCopy = {...appointment, date: formattedDate};
+  const [appointmentData, setAppointmentData] = useState(appointmentCopy);
+  const { date, subject, participants, status, creator, createdAt} = appointmentData;
 
   //TO DO: set a flag for creator to only allow editing if you're the creator
-  //TO DO: handle change
   function handleChange(e) {
     const { name, value } = e.target;
-    setFormData((prevFormData) => ({...prevFormData, [name]: value }));
+    setAppointmentData((prevFormData) => ({...prevFormData, [name]: value }));
   }
 
-  
+  //remember to add onChange for input fields we need to edit
   if (inEditMode) {
     return (
     <form className="flex flex-col p-4 h-full border border-black bg-[#e7e6e1] animate-fadeIn">
       <label htmlFor="date"><u>Date</u>: </label>
-      <input name="date" type="text" value={formattedDate} required/>
+      <input name="date" type="text" value={date} onChange={(e) => handleChange(e)} required/>
       <label htmlFor="subject"><u>Subject</u>: </label>
-      <input name="subject" type="text" value={subject} required/>
+      <input name="subject" type="text" value={subject}  onChange={(e) => handleChange(e)} required/>
       <p className="overflow-hidden whitespace-nowrap text-ellipsis"><u>Participants</u>: {participants.map((participant, i) => {
         if (i === participants.length - 1) return participant;
         return participant + ', ';
@@ -34,7 +35,7 @@ const ViewAndEditAppointment = ({ appointment, inEditMode }) => {
 
   return (
     <div className="p-4 h-full border border-black bg-[#e7e6e1] animate-fadeIn">
-      <p><u>Date</u>: {formattedDate}</p>
+      <p><u>Date</u>: {date}</p>
       <p className="overflow-hidden whitespace-nowrap text-ellipsis"><u>Subject</u>: {subject}</p>
       <p className="overflow-hidden whitespace-nowrap text-ellipsis"><u>Participants</u>: {participants.map((participant, i) => {
         if (i === participants.length - 1) return participant;
