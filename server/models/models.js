01f939f6299f3d79f8e5ebc7aa3d628b67b3e0a7
Ross Terker <ross.terker@gmail.com>
2024-01-02 22:54:34 -0500

    refactor: minor change to potentialDateSchema to allow a single userId per

--- server/models/models.js
+++ server/models/models.js
@@ -23,7 +23,7 @@ const userSchema = new Schema({
 });
 
 const potentialDateSchema = new Schema({
-  userId: [{type: Schema.Types.ObjectId, ref: 'user'}],
+  userId: {type: Schema.Types.ObjectId, ref: 'user'},
   potentialDates: {type: [Date], required: true}
 });
 
